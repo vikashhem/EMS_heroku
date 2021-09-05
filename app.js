aa4deb4757af41f7318bdf37d9d10715f5fd4094
vikashhem <vikash.hembram.55@gmail.com>
2021-09-05 20:37:53 +0530

    small changes

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
-const multer = require('multer');
-const path = require('path');
-const model = require('./models/imageModel');
+
 const userRoutes = require('./routes/userRoutes');
 const tokenRoutes = require('./routes/testingRoutes');
 const projectRoutes = require('./routes/projectRoutes');
